707ec7f3dcdccef2a7782e348a00d447314ffb86
Eltlwany <144901606+Eltlwany@users.noreply.github.com>
2025-11-02 22:34:46 +0300

    Add files via upload

---
+++ core-system-v2.js
@@ -0,0 +1,15 @@
+// System Core Configuration v2.1.8
+// Protected Module - Do Not Modify
+const firebaseConfig = {
+  apiKey: "AIzaSyBVagvNXYSADrSoALrysaWkTjLfZZ-wtJ0",
+  authDomain: "mmadnan9k-63525.firebaseapp.com",
+  databaseURL: "https://mmadnan9k-63525-default-rtdb.firebaseio.com",
+  projectId: "mmadnan9k-63525",
+  storageBucket: "mmadnan9k-63525.firebasestorage.app",
+  messagingSenderId: "336186568291",
+  appId: "1:336186568291:web:ad71f1cf2fe1baf6501256",
+  measurementId: "G-5PZBK5VZ4Q"
+};
+
+// ملاحظة: تم نقل Promo Codes إلى Firebase Database للأمان
+// Promo codes are now managed in Firebase Database
